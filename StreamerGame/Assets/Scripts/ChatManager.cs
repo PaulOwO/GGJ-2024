df322cfa62d7b5373eae296158cbb6ea0d67de4d
Paul <paul.breuza@gmail.com>
2024-01-27 13:24:25 +0100

    arg

--- StreamerGame/Assets/Scripts/ChatManager.cs
+++ StreamerGame/Assets/Scripts/ChatManager.cs
@@ -1,34 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class ChatManager : MonoBehaviour
 {
     public GameObject MessagePrefab;
+
+    private List<GameObject> messages = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        GameObject pref = Instantiate(MessagePrefab, new Vector3(0,0,0), Quaternion.identity);
-        pref.transform.SetParent(this.transform, false);
-
-        pref.transform.position = transform.position;
-        
-        //pref.transform.position = this.transform.position;
-        pref.transform.localScale = new Vector3(1, 1, 1);
-        print("arf");
-        RectTransform rectTransform = this.GetComponent<RectTransform>();
-        
-
-        RectTransform arg = pref.GetComponent<RectTransform>();
-
-        
-        arg.offsetMin = new Vector2(rectTransform.rect.xMin, rectTransform.rect.xMax);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           NewMessage();
+        }
+        UpdateChat();
+    }
+
+    private void UpdateChat()
+    {
+        float currentheight = 0;
+        if (messages.Count > 0)
+        {
+            foreach (GameObject message in messages)
+            {
+                
+                RectTransform transform = message.GetComponent<RectTransform>();
+                transform.anchorMin = Vector2.zero;
+                transform.anchorMax = Vector2.right;
+                 currentheight += transform.rect.height;
+                 if (message == messages[0])
+                 {
+                    currentheight -= transform.rect.height/2; 
+                 }
+                 transform.anchoredPosition = new Vector2(0, currentheight);
+                print(transform.rect.height);
+
+
+                /*RectTransform transform = message.GetComponent<RectTransform>();
+                currentheight = transform.position.y ;
+                transform.anchoredPosition = new Vector2(0, currentheight + messages[messages.Count - 1].GetComponent<RectTransform>().rect.height);*/
+            }
+        }
+        if (messages.Count > 10)
+        {
+            GameObject tmp = messages[10];
+            messages.RemoveAt(10);
+            Destroy(tmp);
+        }
+    }
+    GameObject tmp;
+    private void NewMessage()
+    {
+        tmp = null;
+        int rand = Random.Range(0, 2);
+        tmp = Instantiate(MessagePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        if (rand == 1)
+        {
+            TextMeshProUGUI arg = tmp.GetComponent<TextMeshProUGUI>();
+            arg.SetText("arg ggggggggggggggggggggggggggggggggggg ggggggggggggggggggggggggggggggggggg gggggggggggggggggggggggggggg ggggggggg");
+        }
+        
+        tmp.transform.SetParent(transform, false);
+        tmp.transform.localScale = new Vector3(1, 1, 1);
+        messages.Insert(0,tmp);
         
     }
 }
